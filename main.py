74125b7005f844e1d34288da467e332b08c594d1
dannypeeps <113870485+dannypeeps@users.noreply.github.com>
2024-04-09 17:05:34 -0700

    added home screen and more changes to come

--- main.py
+++ main.py
@@ -5,31 +5,34 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'cs3800!'
 chatapp = SocketIO(app)
 
-#displays the index.html page when the address is set to / (aka default)
-#index.html is our main chatroom page
+# this is the new route for the home screen
 @app.route('/')
-def index():
+def home():
+    return render_template('home.html')
+
+# this is the updated route for the chatroom now under '/chatroom'
+@app.route('/chatroom')
+def chatroom():
     return render_template('index.html')
 
-#recieves the messages and displays them
+usernames = {}
+
+# receives the messages and displays them
 @chatapp.on('message')
 def handle_message(message):
     session_id = request.sid
     username = usernames.get(session_id, 'Anonymous')
-    #shows in terminal
+    # shows in terminal
     print(f'{username}: {message}')
-    #for flask html side
+    # for flask html side
     emit('message', f'{username}: {message}', broadcast=True)
 
-
-usernames = {}
 # Gets username from HTML and binds username to session id in dictionary
 @chatapp.on('set_username')
 def set_username(username):
     session_id = request.sid
     usernames[session_id] = username
 
-
-#runs the flask app
+# runs the flask app
 if __name__ == '__main__':
     chatapp.run(app)
